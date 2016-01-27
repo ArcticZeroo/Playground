554f5e28649439276a92e34be39adc7332c5b6e5
ArcticZeroo <novick.spencer@gmail.com>
2016-01-27 14:23:49 -0500

    Add most validation code

--- encryption/index.php
+++ encryption/index.php
@@ -1,5 +1,34 @@
 <?php
 	$error = "";
+	$username = "";
+	$password = "";
+	$self = htmlspecialchars($_SERVER['PHP_SELF']);
+	$self = ltrim($self);
+	$self = rtrim($self);
+	$self = trim($self);
+	
+	//Validate
+	function validate($input, $type, $maxlength, $minlength){
+		$input = trim($input);
+		$input = stripslashes($input);
+		$input = htmlspecialchars($input);
+		if($input.length() <= $minlength){
+			$error = "Your $type is too short. Minimum length is $minlength characters.";
+		}else if($input.length() >= $maxlength){
+			$error = "Your $type is too long. Maximum length is $maxlength characters.";
+		}else{
+			return $input;
+			//DEBUG
+			$error = "$type works, validated $type: " $input;
+		}
+	}
+	
+	//GET
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+		validate($username, "username", 8, 24);
+	}
 ?>
 <html>
 	<head>
@@ -7,11 +36,13 @@
 		<link rel="stylesheet" type="text/css" href="stylesheets/index.css"/>
 	</head>
 	<body>
-		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-			<input type='text' name='username' placeholder='Username'><br>
-			<input type='password' name='password' placeholder='Password'><br>
-			<input type='submit' value='Submit'>
-		</form>
-		<div id="error"><?php if($error != ""){echo $error;} ?></div>
+		<div id="content">
+			<form action="<?php echo ; ?>" method="post">
+				<input type='text' name='username' placeholder='Username'><br>
+				<input type='password' name='password' placeholder='Password'><br>
+				<input type='submit' value='Submit'>
+			</form>
+			<div id="error"><?php if($error != ""){echo $error;} ?></div>
+		</div>
 	</body>
 </html>
